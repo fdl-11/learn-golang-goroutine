715854562d9b069a531e7ed63207240ea6d36bf3
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-31 13:11:21 +0700

    range channel

--- channel_test.go
+++ channel_test.go
@@ -7,6 +7,7 @@ package golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -116,4 +117,21 @@ func TestBufferedChannel(t *testing.T) {
 	time.Sleep(2 * time.Second)
 
 	fmt.Println("Selesai!")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke-" + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
 }
